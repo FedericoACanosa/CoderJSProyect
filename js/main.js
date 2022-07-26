0df7eba0117944e0aa47cd19663af92d5b02b091
FedericoCanosa <federicoadrielcanosa@gmail.com>
2022-07-26 09:41:11 -0300

    Test1

---
+++ js/main.js
@@ -0,0 +1,54 @@
+
+let limite = parseInt(prompt("Ingresa un número"))
+
+for (let i = 0; i < limite; i++){
+
+    console.log("Iteración Nro,", i);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// debugger
+
+// let nivel = parseInt(prompt ("Ingrese su nivel"))
+
+// if (nivel <15){
+//     alert("Sigue completando partidas contra IA")
+// }
+// else if (nivel < 80){
+//     alert("Puedes comenzar a jugar contra otros jugadores en partidas NO CLASIFICATORIAS")
+
+// }
+// else if (nivel > 100){
+//     alert("Nivel apto para partidas CLASIFICATORIAS")
+
+// }
+// else {
+//     alert("Nivel máximo, todas funcionabilidades desbloqueadas.")
+// }
